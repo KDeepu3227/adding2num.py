c2ab608457361717700a936bb568076793a80c69
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 20:01:33 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+###
+create a list with 5 string elements print the last two elements using slicing technique?
+###
+li = ['sumiya','bharathi','sandhya','sreenu','deepu']
+print(li[3:5])
+###
+sreenu,deepu
