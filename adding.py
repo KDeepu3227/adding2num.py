bffd7ece291c28a5d143ce627ce2a637f60c7598
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:30:08 +0530

    Create adding.py

---
+++ adding.py
@@ -0,0 +1,10 @@
+'''
+write a python program to take for adding 2 numbers from keyboard?
+'''
+x = int(input("enter the 1st number"))
+y = int(input("enter the 2nd number"))
+print(x+y)
+'''
+1st number:20
+2nd number:25
+x+y = 45
