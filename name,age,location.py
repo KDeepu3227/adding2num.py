cc815b2d2d5d84186e3e763a7d84ef9920ba353b
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:36:18 +0530

    Create name,age,location.py

---
+++ name,age,location.py
@@ -0,0 +1,11 @@
+###
+write a program name,age,and location from keyboard print values?
+###
+name = input("enter the name")
+age = int(input("enter the age"))
+location = input("enter the location")
+print(name,age,location)
+###
+name = deepika
+age= 19
+location = chowdepalli
