c8cef663e960412e0402bb499c0359393f31c24b
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:53:28 +0530

    Create oddevenindex.py

---
+++ oddevenindex.py
@@ -0,0 +1,12 @@
+###
+print the character at odd index and even index?
+###
+st = input("enter the string")
+st = chennai
+print(st[0:7:2])
+print(st[0::2])
+print(st[1::2])
+###
+ceni
+ceni(even)
+hna(odd)
