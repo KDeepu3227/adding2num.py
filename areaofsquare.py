0c0b44888e76cbc5cf2e798960b495b5b88378e7
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:40:23 +0530

    Create areaofsquare.py

---
+++ areaofsquare.py
@@ -0,0 +1,9 @@
+### square
+write a python program for area of square?
+###
+len = int(input("enter the area of square"))
+print(len**2)
+###
+area of square = 4
+               =4**2
+               =16
