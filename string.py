e50532ac949e37ab0b85ac248b7fae1a901ea552
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:50:08 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,10 @@
+###
+take a string as input from kb print last 5 characters and negative index &using len function?
+###
+st = input("enter the string:")
+print(st[len(st)-5:)
+print(st[-5:])
+###
+st =deepika
+    epika
+    epika
