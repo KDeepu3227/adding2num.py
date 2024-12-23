8c66fa4c55f6dce04f0fa9d9d4d0b35d75538101
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:43:17 +0530

    Create areaofrectangle.py

---
+++ areaofrectangle.py
@@ -0,0 +1,10 @@
+### rectangle
+write a python program for area of rectangle?
+###
+x = int(input("enter the length of rectangle"))
+y = int(input("enter the breadth of rectangle"))
+print(x*y)
+###
+length = 6
+breadth = 3
+ 18
