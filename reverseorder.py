1ed9e564b0d53f22ee2cea7d27f0db0f0c017cea
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 19:56:46 +0530

    Create reverseorder.py

---
+++ reverseorder.py
@@ -0,0 +1,8 @@
+###
+take a string as input from the kb print the element in reverse order starting from the last index with a difference of two
+ex:palamaneru
+###
+st = 'palamaneru'
+print(st[::-2])
+###
+u e a a a
